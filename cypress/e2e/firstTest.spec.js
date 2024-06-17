203c56fe975609c133b0ccbd957c2eff8fcb6112
Bob <>
2024-06-17 21:47:57 +0300

    Finding web elements

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -33,4 +33,29 @@ describe('First test suite', () => {
         //by cypress test ID
         cy.get('[data-cy="imputEmail1"]')
     })
+
+    it.only('second test', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //3 main methods
+        //get() - find elements on the page by locator globally
+        //find() - find child elements by locator
+        //contains() - find HTML text and be text and locator
+
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.contains('nb-card','Horizontal form').find('button')
+        cy.contains('nb-card','Horizontal form').contains('Sign in')
+
+        //cypress chains and DOM
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+    })
 })
