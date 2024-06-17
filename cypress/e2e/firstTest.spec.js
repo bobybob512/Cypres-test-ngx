b7ad00d165b03c6ff4a37edc6f1f3c824316a29d
Bob <>
2024-06-17 20:52:13 +0300

    types of locators

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -2,7 +2,35 @@
 
 describe('First test suite', () => {
     it('First test', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
         //by tag name
         cy.get('input')
+
+        //by id
+        cy.get('#inputEmail1')
+
+        //by class
+        cy.get('.input-full-width')
+
+        //by attribute name
+        cy.get('[fullwidth]')
+
+        //by attribute and value
+        cy.get('[placeholder="Email"]')
+
+        //by entire class name
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        //by two attributes
+        cy.get('[placeholder="Email"][fullwidth]')
+
+        //by tag, class, id and attribute
+        cy.get('input.input-full-width#inputEmail1[fullwidth]')
+
+        //by cypress test ID
+        cy.get('[data-cy="imputEmail1"]')
     })
 })
