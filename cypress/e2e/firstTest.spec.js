3a007197476ca7f0352abf21737e49ddc5599363
Bob <>
2024-06-23 17:02:32 +0300

    list and dropdowns

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -147,7 +147,7 @@ describe('First test suite', () => {
         cy.get('[type="checkbox"]').eq(1).click({force: true})
     })
 
-    it.only('datepicker', () => {
+    it('datepicker', () => {
 
         function selectDayFromCurrent(day){
 
@@ -188,7 +188,29 @@ describe('First test suite', () => {
             cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
             // or
             cy.wrap(input).should('have.value', dateToAssert)
+        })  
+    })
 
+    it.only('list and dropdowns', () => {
+        cy.visit('/')
+        //cy.get('nav').find('nb-select').click()
+        //or
+        // 1
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+
+        // 2
+        cy.get('nav nb-select').then(dropDown => {
+            cy.wrap(dropDown).click()
+            cy.get('.options-list nb-option').each( (listItem, index) => {
+                const itemText = listItem.text().trim()
+                cy.wrap(listItem).click()
+                cy.wrap(dropDown).should('contain', itemText)
+                if(index < 3){
+                    cy.wrap(dropDown).click()
+                }
+            })
         })
     })
 })
