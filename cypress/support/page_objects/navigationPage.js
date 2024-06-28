c61eea467b3c8d85f891f1a36ddc516632aa3a3b
Bob <>
2024-06-28 15:29:12 +0300

    added parameter to selectGroupMenuItem function

--- cypress/support/page_objects/navigationPage.js
+++ cypress/support/page_objects/navigationPage.js
@@ -1,4 +1,4 @@
-function selectGroupMenuItem(){
+function selectGroupMenuItem(groupName){
     cy.contains('a', 'Forms').then( menu => {
         cy.wrap(menu).find('.expand-state g g').invoke('attr', 'data-name').then(attr=> {
             if(attr.includes('left')){
@@ -11,15 +11,15 @@ function selectGroupMenuItem(){
 export class NavigationPage{
     
     formLayoutsPage(){
-        //cy.contains('Forms').click()
+        
 
-        selectGroupMenuItem()
+        selectGroupMenuItem('Forms')
         cy.contains('Form Layouts').click()
     }
 
     datepickerPage(){
-        //cy.contains('Forms').click()
-        selectGroupMenuItem()
+        
+        selectGroupMenuItem('Forms')
         cy.contains('Datepicker').click()
     }
 }
